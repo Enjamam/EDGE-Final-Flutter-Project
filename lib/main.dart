d2e482fe45e4f6755da2da5e0c954bdbb83f09fa
Emrus <emjamamulemrus@gmail.com>
2025-01-09 12:19:01 +0600

    First release of the demo project

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shoe_ui/pages/homepage.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,42 +10,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Colors.teal[200],
-        appBar: AppBar(
-          title: Text(
-            'Love Bar',
-            style: TextStyle(
-              color: Colors.white,
-              fontSize: 30.0,
-            ),
-          ),
-          centerTitle: true,
-          backgroundColor: Colors.teal[500],
-          elevation: 0.0,
-          leading: Icon(Icons.menu),
-          //actionsIconTheme: IconThemeData(color: Colors.white),
-          actions: [IconButton(icon: Icon(Icons.logout), onPressed: () {})],
-        ),
-        body: Center(
-          child: Container(
-            height: 300.0,
-            width: 300.0,
-            decoration: BoxDecoration(
-              color: Colors.blue,
-              borderRadius: BorderRadius.circular(360.0),
-              border: Border.all(
-                color: Colors.white,
-                width: 5.0,
-              ),
-            ),
-            padding: EdgeInsets.all(30.0),
-            child: Icon(Icons.favorite, color: Colors.red, size: 100.0),
-          ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
